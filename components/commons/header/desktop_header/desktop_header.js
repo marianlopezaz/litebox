0b026439b3d9151f21c617c3cfbb09d6e9257235
mariano <mariano.lopez@almundo.com>
2020-10-25 15:20:14 -0300

    header responsive listo, falta header mobile

--- components/commons/header/desktop_header/desktop_header.js
+++ components/commons/header/desktop_header/desktop_header.js
@@ -1,42 +1,13 @@
-import { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { IconButton, useMediaQuery, useTheme } from '@material-ui/core';
-import ProfileMenu from '../profile_menu/profile_menu';
-
+import LeftHeaderSection from './left_header_section/left_header_section';
+import RightHeaderSection from './right_header_section/right_header_section';
 
 const DesktopHeader = () => {
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down(1065));
-    const [hover, setHover] = useState();
-    const [exploreOptions, setExploreOptions] = useState();
-
-
-    const handleHover = () => {
-        setHover(!hover);
-    }
-
-    useEffect(()=>{
-        setExploreOptions(fullScreen);
-    },[fullScreen]);
 
     return (
         <div className={styles.header_container}>
- 
-
-            <div className={styles.secondary_nav}>
-                <div className={styles.text_children_container}>
-                    <span>Niños</span>
-                </div>
-
-                    <IconButton>
-                        <NotificationsIcon style={{ color: 'var(--white)' }} />
-                    </IconButton>
-                    <div className={styles.new_notification_alert}></div>
-                    <div className={styles.profile_container}>
-                        <ProfileMenu />
-                    </div>
-            </div>
+            <LeftHeaderSection />
+            <RightHeaderSection />
         </div>
     )
 }
---
+++ components/commons/header/desktop_header/explore_option_component/explore_options_component.js
@@ -0,0 +1,60 @@
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import styles from './styles.module.scss';
+import { useState } from 'react';
+import LiteflixAvatar from '../../../liteflix_avatar_component/liteflix_avatar_component';
+import { Collapse } from '@material-ui/core';
+import Modal from '../../../modal/modal';
+import AddMovieForm from '../../../../movies/add_movie_form/add_movie_form';
+import AddMovieButtonComponent from '../../../buttons/add_movie_button_component/add_movie_button_component';
+
+const ExploreOptionsComponent = () => {
+
+    const [moreOptions, setMoreOptions] = useState();
+
+    const handleMoreOptions = () => {
+        setMoreOptions(!moreOptions);
+    }
+
+    return (
+        <>
+            <div className={styles.container} onClick={handleMoreOptions}>
+                <span className={styles.explore_text}>Explorar</span>
+                {!moreOptions ?
+                    <KeyboardArrowDownIcon style={{ color: 'var(--white)' }} /> :
+                    <KeyboardArrowUpIcon style={{ color: 'var(--white)' }} />}
+            </div>
+
+            <Collapse in={moreOptions} className={styles.collapse_container}>
+                <div className={styles.top_adornment}></div>
+                <ul className={styles.explore_list}>
+                    <li className={styles.explore_list_item}>
+                        Inicio
+                    </li>
+                    <li className={styles.explore_list_item}>
+                        Series
+                    </li>
+                    <li className={styles.explore_list_item}>
+                        Películas
+                    </li>
+                    <li className={styles.explore_list_item}>
+                        Agregados recientemente
+                    </li>
+                    <li className={styles.explore_list_item}>
+                        Mi lista
+                    </li>
+                    <li className={styles.container_li}>
+                        <Modal
+                            title={"Agregar nueva película"}
+                            body={<AddMovieForm />}
+                            button={<AddMovieButtonComponent />}
+                        />
+                    </li>
+                </ul>
+            </Collapse>
+        </>
+    )
+
+}
+
+export default ExploreOptionsComponent;
