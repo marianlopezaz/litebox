5961c0090d46b208fc39d0187c92a3412da89bf9
mariano <mariano.lopez@almundo.com>
2020-11-01 21:17:28 -0300

    arreglos

--- components/commons/skeletons/info_component_skeleton.js
+++ components/commons/skeletons/info_component_skeleton.js
@@ -2,12 +2,10 @@ import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 
 const InfoComponentSkeleton = () => {
     return (
-        <SkeletonTheme color='#b9b9b9'  highlightColor="#a3a7aa">
+        <SkeletonTheme color='#b9b9b9' highlightColor="#a3a7aa">
             <Skeleton count={1} />
             <Skeleton count={1} height={100} />
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'center' }}>
-                <Skeleton count={1} height={40} width={185}/> <Skeleton count={1} height={40} width={185}/>
-            </div>
+            <Skeleton count={1} height={40} width={185} />
             <Skeleton count={1} />
             <Skeleton count={1} />
             <Skeleton count={1} />
